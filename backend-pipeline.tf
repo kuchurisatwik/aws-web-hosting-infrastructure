9cce299b38f107a9ab93b3d77cece7a334504cc9
kuchurisatwik <kuchurisatwik@gmail.com>
2025-12-15 11:50:57 +0530

    dlkfsldj

--- backend-pipeline.tf
+++ backend-pipeline.tf
@@ -90,7 +90,7 @@ resource "aws_codedeploy_deployment_group" "backend_dg" {
 }
 
 resource "aws_codebuild_project" "backend_codebuild_project" {
-  name          = "backend-build"
+  name          = "backend-build-new"
   service_role  = aws_iam_role.codebuild_role.arn
   build_timeout = "5"
   source {
