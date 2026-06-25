8571cadfc822ce843fc43a8eb4423662db2daa94
Satwik Kuchuri <kuchurisatwik@gmail.com>
2026-06-25 16:34:31 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,5 @@
 
 
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-  tags = { Name = "tf-public-rt" }
-}
-
 resource "aws_route_table_association" "public_assoc" {
   count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
