73a21abf78885bb0b12dc33eb787515d752b513f
Satwik Kuchuri <kuchurisatwik@gmail.com>
2026-06-25 16:03:52 +0530

    Remove public subnet resource from vpc.tf
    Removed public subnet resource definition from vpc.tf.

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,5 @@
 
 
-resource "aws_subnet" "public" {
-  count                   = length(var.public_subnet_cidrs)
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnet_cidrs[count.index]
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "tf-public-${count.index + 1}"
-  }
-}
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
