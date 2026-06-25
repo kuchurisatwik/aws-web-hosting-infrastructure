452f2fb2a5e143c1ff223103a491a2f09946e1fc
Satwik Kuchuri <kuchurisatwik@gmail.com>
2026-06-25 20:50:29 +0530

    Add AI_REPORT for 8381c65

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,37 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "test-vpc"
+  }
+}
 
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = "us-east-1a" # Assuming us-east-1a for simplicity
+  tags = {
+    Name = "test-public-subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "test-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "test-public-rt"
+  }
+}
 
 resource "aws_route_table_association" "public_assoc" {
   count          = length(aws_subnet.public)
